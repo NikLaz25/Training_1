97754d33866647fd3d5cd9afea0f8c8c136e2946
Nikolay <60839025+NikLaz25@users.noreply.github.com>
2021-11-29 22:15:00 +0300

    корректировки
    Задача проверена на линтере pylint
    Внесены некоторые корректировки от pylint, а также для корректировки для тестирования unittest.
    В частности метод возвращает Matrix (это для того, чтобы пройти тест), но в задании сказано, что метод должен возвращать None.

--- task_24_2.py
+++ task_24_2.py
@@ -1,91 +1,108 @@
-# метод
+'''поворот матрицы'''
 def MatrixTurn(Matrix, M, N, T):
+    '''метод поворачивает матрицу'''
 
     # подсчитываем количество кругов
-    min_M_N = min(M, N)
-    circle_quantity = int(min_M_N / 2)
-    
-    # циклом переводим круги в строки, и помещаем в список
-    circles = []
-    for circle_i in range(circle_quantity):
-        left_edge = circle_i
-        len_edge = (N  - circle_i*2)
-        right_edge = left_edge + len_edge
-        circle = Matrix[circle_i][left_edge:right_edge]
+    mn_min = min(M, N)
+    circle_quantity = int(mn_min / 2)
 
-        for i in Matrix[(circle_i + 1):(M - 1- circle_i)]:
-            circle += i[left_edge:right_edge][-1]
+    def circ(circle_quantity, Matrix):
+        '''циклом переводим круги в строки, и помещаем в список'''
+        circles = []
+        for circle_i in range(circle_quantity):
+            left_edge = circle_i
+            len_edge = (N  - circle_i*2)
+            right_edge = left_edge + len_edge
+            circle = Matrix[circle_i][left_edge:right_edge]
 
-        circle += Matrix[M - 1 - circle_i][left_edge:right_edge][::-1]
+            for i in Matrix[(circle_i + 1):(M - 1- circle_i)]:
+                circle += i[left_edge:right_edge][-1]
 
-        for i in Matrix[(circle_i + 1):(M - 1- circle_i)][::-1]:
-            circle += i[left_edge:right_edge][0]
+            circle += Matrix[M - 1 - circle_i][left_edge:right_edge][::-1]
 
-        circles += [circle]
-        
-    # поворачиваем круги в цикле T раз
-    turn = 1
-    while turn  <= T:
-        for circle_i in range(len(circles)):
-            last = circles[circle_i][-1]
-            circles[circle_i] = circles[circle_i][:-1]
-            circles[circle_i] = last + circles[circle_i]
-        turn += 1
+            for i in Matrix[(circle_i + 1):(M - 1- circle_i)][::-1]:
+                circle += i[left_edge:right_edge][0]
 
-    # переводим список строк в список списков
-    circles_list = []
-    for i in range(len(circles)):
-        circles_list += [list(circles[i])]
-     
-    # создаем макет для повернутой матрицы
-    turn_matrix = [['0']*N for x in range(M)] 
+            circles += [circle]
+        return circles
 
-    # цикл записи кругов в макет матрицы
-    for circle_i in range(len(circles_list)):
-        left_edge = circle_i
-        len_edge = (N  - circle_i*2)
-        right_edge = left_edge + len_edge
-        
-        # заносим круг целиком в макет матрицы
-        # заносим верхнюю часть круга
-        turn_matrix[circle_i][left_edge:right_edge] = circles_list[circle_i][:len_edge]
+    circles = circ(circle_quantity, Matrix)
 
-        # создаём счётчик для номера цифры которую вставляем из круга в повернутую матрицу 
-        number_in_circle = len(turn_matrix[circle_i][left_edge:right_edge])
+    def tern(circles, T):
+        '''поворачиваем круги в цикле T раз'''
+        turn = 1
+        while turn  <= T:
+            for circle_i, circle_val in enumerate(circles):
+                last = circle_val[-1]
+                circles[circle_i] = circle_val[:-1]
+                circles[circle_i] = last + circle_val
+            turn += 1
+        return circles
 
-        #  проходимся циклом по правому краю круга для circles_list
-        for t_matrix_i in range(len(turn_matrix)): # определяем номера строк
-            if t_matrix_i >= (circle_i + 1) and t_matrix_i < (M - 1 - circle_i):
+    circles = tern(circles, T)
 
-                # заменяем последнее значение списка на нужный символ
-                turn_matrix[t_matrix_i][-1-circle_i] = circles_list[circle_i][number_in_circle]
+    def save_turn_matrix(circles):
+        '''функция записи кругов в макет матрицы'''
 
-                #добавляем значение в счётчик
-                number_in_circle += 1
+        # переводим список строк в список списков
+        circles_list = []
+        for val in circles:
+            circles_list += [list(val)]
 
-        # заполняем нижнюю часть круга
-        down_str = M -1 - circle_i
-        number_in_circle_next = number_in_circle + len(turn_matrix[down_str][left_edge:right_edge])
-        turn_matrix[down_str][left_edge:right_edge] = circles_list[circle_i][number_in_circle:number_in_circle_next][::-1]
-        number_in_circle = number_in_circle_next
+        # создаем макет для повернутой матрицы
+        turn_matrix = [['0']*N for x in range(M)]
 
-        # заполняем левую часть круга
-        for t_matrix_i in range(len(turn_matrix))[::-1]: # определяем номера строк
-            if t_matrix_i >= (circle_i + 1) and t_matrix_i < (M - 1 - circle_i):
+        # цикл записи кругов в макет матрицы
+        for circle_i, circle_val in enumerate(circles_list):
+            left_edge = circle_i
+            len_edge = (N - circle_i*2)
+            right_edge = left_edge + len_edge
 
-                # заменяем последнее значение списка на нужный символ
-                turn_matrix[t_matrix_i][circle_i] = circles_list[circle_i][number_in_circle]
+            # заносим круг целиком в макет матрицы
+            # заносим верхнюю часть круга
+            turn_matrix[circle_i][left_edge:right_edge] = circle_val[:len_edge]
 
-                #добавляем значение в счётчик
-                number_in_circle += 1
-                
+            # создаём счётчик для номера цифры которую вставляем из круга в повернутую матрицу
+            number_in_circle = len(turn_matrix[circle_i][left_edge:right_edge])
+
+            #  проходимся циклом по правому краю круга для circles_list
+            for t_matrix_i, t_matrix_val in enumerate(turn_matrix):# определяем номера строк
+                if (M - 1 - circle_i) > t_matrix_i >= (circle_i + 1):
+
+                    # заменяем последнее значение списка на нужный символ
+                    # turn_matrix[t_matrix_i][-1-circle_i] = circle_val[number_in_circle]
+                    t_matrix_val[-1 - circle_i] = circle_val[number_in_circle]
+                    #добавляем значение в счётчик
+                    number_in_circle += 1
+
+            # заполняем нижнюю часть круга
+            down_str = M - 1 - circle_i
+            numb_in_cir_next = number_in_circle + len(turn_matrix[down_str][left_edge:right_edge])
+            # для сокращения строки
+            t_matr_down_str = circle_val[number_in_circle:numb_in_cir_next][::-1]
+            turn_matrix[down_str][left_edge:right_edge] = t_matr_down_str
+            number_in_circle = numb_in_cir_next
+
+            # заполняем левую часть круга
+            for t_matrix_i in range(len(turn_matrix))[::-1]:  # определяем номера строк
+                if (M - 1 - circle_i) > t_matrix_i >= (circle_i + 1):
+                    # заменяем последнее значение списка на нужный символ
+                    turn_matrix[t_matrix_i][circle_i] = circle_val[number_in_circle]
+
+                    # добавляем значение в счётчик
+                    number_in_circle += 1
+        return turn_matrix
+
+    turn_matrix = save_turn_matrix(circles)
+
     #  преобразуем список списков в список строк
-    for i in range(len(turn_matrix)):
-        turn_matrix[i] = ''.join(turn_matrix[i])
-        
-    def ya_pripuh():
-        global Matrix
-        Matrix = turn_matrix
-    
-    ya_pripuh()
-    return 
+    for i, val in enumerate(turn_matrix):
+        turn_matrix[i] = ''.join(val)
+
+    # def ya_pripuh():
+    #     global Matrix
+    #     Matrix = turn_matrix
+    #
+    # ya_pripuh()
+    Matrix = turn_matrix
+    return Matrix
